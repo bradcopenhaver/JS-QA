adbc57fd774d216753bc31e6f195be2bb85e3c6c
bradcopenhaver <bradcopenhaver@gmail.com>
2017-01-13 12:32:57 -0800

    fix answer save function so that questions link to answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -7,7 +7,11 @@ export default Ember.Route.extend({
   actions: {
     saveAnswer(params) {
       var newAnswer = this.store.createRecord('answer', params);
-      newAnswer.save();
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
       this.transitionTo('index');
     }
   }
