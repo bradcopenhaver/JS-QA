c1da4863cea5fc43ed871b43fc776007467e0810
bradcopenhaver <bradcopenhaver@gmail.com>
2017-01-13 15:44:29 -0800

    adjust styling and add cancel button to edit form

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -6,11 +6,17 @@ export default Ember.Component.extend({
     editQuestionForm() {
       this.set('editQuestionForm', true);
     },
+    cancel() {
+      this.set('editQuestionForm', false);
+      this.set('author', "");
+      this.set('text', "");
+      this.set('notes', "");
+    },
     updateQuestion(question) {
       var params = {
         author: this.get('author'),
         text: this.get('text'),
-        sources: this.get('sources')
+        sources: this.get('notes')
       };
       this.set('updateQuestionForm', false);
       this.sendAction('updateQuestion', question, params);
